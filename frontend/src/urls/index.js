43e381479280a1461b1114c356db5062ded69a32
House-lovers7 <taisuke.got@gmail.com>
2022-03-08 13:21:23 +0900

    デフォルトURLの変更リセット

--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -1,5 +1,4 @@
 const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1'
-
 export const restaurantsIndex = `${DEFAULT_API_LOCALHOST}/restaurants`
 export const foodsIndex = (restaurantId) =>
   `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`
